b92072f05bf8cb54df69f8922480752dca766a41
Magomedali <web-ali@yandex.ru>
2019-10-12 07:08:22 +0300

    api nginx configure

--- api/src/Http/Action/HomeAction.php
+++ api/src/Http/Action/HomeAction.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
-
 namespace Api\Http\Action;
+
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
