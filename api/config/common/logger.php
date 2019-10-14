b87d9c4c8a85466b8fb319460fd9d67c0d03377e
Magomedali <web-ali@yandex.ru>
2019-10-14 13:45:58 +0300

    Install logger

---
+++ api/config/common/logger.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+use Psr\Log\LoggerInterface;
+use Psr\Container\ContainerInterface;
+use Api\Infrastructure\Framework\ErrorHandler\LogHandler;
+use Api\Infrastructure\Framework\ErrorHandler\LogPhpHandler;
+
+return [
+
+	LoggerInterface::class => function (ContainerInterface $container) {
+		$confir = $container->get('config')['logger'];
+		$logger = new \Monolog\Logger('API');
+		$logger->pushHandler(new \Monolog\Handler\StreamHandler($config['file']));
+		return $logger;
+	},
+
+	'errorHandler' => function (ContainerInterface $container) {
+        return new LogHandler(
+            $container->get(LoggerInterface::class),
+            $container->get('settings')['displayErrorDetails']
+        );
+    },
+    
+    'phpErrorHandler' => function (ContainerInterface $container) {
+        return new LogPhpHandler(
+            $container->get(LoggerInterface::class),
+            $container->get('settings')['displayErrorDetails']
+        );
+    },
+
+	'config' => [
+        'logger' => [
+            'file' => 'var/log/app.log',
+        ]
+    ]
+];
